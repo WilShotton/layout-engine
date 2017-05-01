0760b042d02f7c5e8a61d5cb7ef0cc65caf3ccf2
Wil Shotton <wil.shotton@gmail.com>
2017-05-01 15:17:43 +0100

    Fix flashing scrollbar

--- src/layout-engine/layout-group.jsx
+++ src/layout-engine/layout-group.jsx
@@ -79,8 +79,6 @@ class LayoutGroup extends RxComponent{
                 this.props$
                     .map(({bounds, layout}) => () => {
 
-                        console.log('bounds', bounds)
-
                         const childHeight = bounds.height / _.size(layout.children)
 
                         return {values: _.map(layout.children, child => {
@@ -115,177 +113,50 @@ class LayoutGroup extends RxComponent{
                                 }
                             })
                     })
-                    // .do(() => {
-                    //
-                    //     const min = 20
-                    //     const items = [40, 80, 10]
-                    //     const total = 100
-                    //
-                    //
-                    //     const add = (a, b) => a + b
-                    //
-                    //
-                    //     const freeItems = _.filter(items, item => item > min)
-                    //     const freeItemCount = _.size(freeItems)
-                    //     const freeItemTotal = _.reduce(freeItems, add, 0)
-                    //
-                    //     const fixed = (_.size(items) - freeItemCount) * min
-                    //
-                    //     const outstanding = total - fixed
-                    //
-                    //     const out = _.map(items, item => {
-                    //
-                    //         return Math.max(min, item / freeItemTotal * outstanding)
-                    //     })
-                    //
-                    //
-                    // })
                     .withLatestFrom(
                         this.propAsStream('bounds'),
                         ({index, x, y}, {width, height}) => ({snapshot, values}) => {
 
-                            // const foo = () => {
-                            //
-                            //     const add = (acc, {measure}) => acc + measure
-                            //
-                            //     const initial = _.slice(values, 0, index + 1)
-                            //     // console.log('initial', initial)
-                            //
-                            //     const sliced = _.slice(values, index + 1)
-                            //     // console.log('sliced', sliced)
-                            //
+                            const add = (acc, {measure}) => acc + measure
 
+                            const before = _.slice(values, 0, index)
 
-                            //     const freeItems = _.filter(sliced, item => item.measure > MIN_MEASURE)
-                            //     // console.log('freeItems', freeItems)
-                            //
-                            //     const freeItemCount = _.size(freeItems)
-                            //     // console.log('freeItemCount', freeItemCount)
-                            //
-                            //     const freeItemTotal = 600 - _.reduce(_.slice(snapshot, 0, index + 1), add, 0) - y
-                            //
-                            //     // const freeItemTotal = _.reduce(freeItems, add, 0)
-                            //     // console.log('freeItemTotal', freeItemTotal)
-                            //
-                            //     const fixed = (_.size(sliced) - freeItemCount) * MIN_MEASURE
-                            //     // console.log('fixed', fixed)
-                            //
-                            //     const outstanding = _.reduce(sliced, add, 0) - fixed
-                            //     // console.log('outstanding', outstanding)
-                            //
-                            //     return _.concat(initial, _.map(sliced, item => {
-                            //
-                            //         return {
-                            //             ...item,
-                            //             measure: Math.max(MIN_MEASURE, item.measure / freeItemTotal * outstanding)
-                            //         }
-                            //     }))
-                            //
-                            //
-                            // }
-                            //
-                            // console.log('out', foo())
-
-                            const bar = () => {
-
-                                const add = (acc, {measure}) => acc + measure
-
-
-                                const before = _.slice(values, 0, index)
-
-                                const current = {
-                                    ...values[index],
-                                    measure: Math.max(
-                                        snapshot[index].measure + y,
-                                        MIN_MEASURE
-                                    )
-                                }
-
-                                const after = _.slice(values, index + 1)
-
-
-                                // const fixed = _.filter(after, item => item.measure <= MIN_MEASURE)
-                                //
-                                // const fixedCount = _.size(fixed)
-                                //
-                                // const fixedMeasure = fixedCount * MIN_MEASURE
-                                // console.log('fixedMeasure', fixedMeasure)
-
-
-                                const initialAfterHeight = _.reduce(after, add, 0)
-
-                                const updatedAfterHeight = height - _.reduce(before, add, 0) - current.measure
-
-                                return _.concat(before, current, _.map(after, value => {
-
-                                    return {
-                                        ...value,
-                                        measure: Math.max(
-                                            MIN_MEASURE,
-                                            Math.floor(value.measure  / initialAfterHeight * updatedAfterHeight)
-                                        )
-                                    }
-                                }))
-                                // .map(item => ({...item, measure: Math.floor(item.measure)}))
-                            }
-
-                            const res = () => {
-
-                                const sliced = _.slice(values, index + 1)
-                                // console.log('sliced', sliced)
-
-                                const totalHeight = _.reduce(sliced, (acc, {measure}) => {
-
-                                    return acc + measure
-
-                                }, 0)
-
-                                const targetMeasure = Math.max(
+                            const current = {
+                                ...values[index],
+                                measure: Math.max(
                                     snapshot[index].measure + y,
                                     MIN_MEASURE
                                 )
+                            }
+
+                            const after = _.slice(values, index + 1)
+
+                            const initialAfterHeight = _.reduce(after, add, 0)
+                            const updatedAfterHeight = height - _.reduce(before, add, 0) - current.measure
+
+                            const update = _.concat(before, current, _.map(after, value => {
+
+                                const measure = Math.floor(value.measure  / initialAfterHeight * updatedAfterHeight)
 
                                 return {
-                                    snapshot,
-                                    values: _.map(values, (value, i) => {
+                                    ...value,
+                                    measure: Math.max(MIN_MEASURE, measure)
+                                }
+                            }))
 
-                                        if (i < index || targetMeasure === MIN_MEASURE) {
-                                            return value
-                                        }
+                            // NOTE: The following fixes an offset that occurs when resizing at speed
+                            const total = _.reduce(update, (acc, {measure}) => acc + measure, 0)
 
-                                        if (i === index) {
+                            if (total > height) {
 
-                                            return {
-                                                ...value,
-                                                measure: targetMeasure
-                                            }
-                                        }
+                                const last = _.find(update, ({measure}) => measure > MIN_MEASURE)
 
-                                        const pc = value.measure / totalHeight
-
-                                        return {
-                                            ...value,
-                                            measure: Math.max(
-                                                snapshot[i].measure - (y * pc),
-                                                MIN_MEASURE
-                                            )
-                                        }
-                                    })
+                                if (last) {
+                                    last.measure -= total - height
                                 }
                             }
 
-                            // console.log('res', _.map(res().values, 'measure'))
-                            console.log('bar', _.map(bar(), 'measure'))
-
-                            const total = _.map(bar(), 'measure').reduce((a, b) => a + b, 0)
-                            if (total > 600) {
-
-                                console.warn('bar total', total)
-                            }
-
-                            console.log(' ')
-
-                            return {snapshot, values: bar()}
+                            return {snapshot, values: update}
                         }
                     )
             )
