9a39c6ce05ece98272f502b339aec6478505ee0f
Wil Shotton <wil.shotton@gmail.com>
2017-05-14 08:52:58 +0100

    Investigate nested layout config

--- src/harness/vertical-layout.jsx
+++ src/harness/vertical-layout.jsx
@@ -2,6 +2,52 @@ import First from './components/first'
 import Second from './components/second'
 import Third from './components/third'
 
+
+const layout = {
+
+    factory: 'HorizontalLayout',
+
+    children: [
+
+        {
+            factory: 'Navigation',
+            measure: 60,
+
+        }, {
+
+            factory: 'VerticalLayout',
+
+            children: [
+                {
+
+                    factory: 'Header',
+                    measure: 60
+
+                }, {
+
+                    factory: 'RoutedLayout',
+
+                    children: [
+
+                        {
+                            factory: 'First'
+                        }, {
+                            factory: 'Second'
+                        }, {
+                            factory: 'Third'
+                        }
+                    ]
+
+                }, {
+
+                    factory: 'Footer',
+                    measure: 60
+                }
+            ]
+        }
+    ]
+}
+
 export default {
 
     layout: 'vertical',
